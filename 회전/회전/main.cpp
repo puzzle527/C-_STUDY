9fedb69342c30ba45c88f2065bfd238776fea520
32150685 <32150685@dankook.ac.kr>
2021-02-16 17:35:18 +0900

    commmmmmm

---
+++ 회전/회전/main.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#define Size 4
+using namespace std;
+
+int main(void)
+{
+	int Input;
+	int arrA[Size][Size];
+	int arrB[Size][Size];
+
+	for (int i = 0; i < Size; i++)
+	{
+		for (int j = 0; j < Size; j++)
+		{
+			arrA[i][j] = Size * i + j;
+		}
+	}
+	while (1)
+	{
+		for (int i = 0; i < Size; i++)
+		{
+			for (int j = 0; j < Size; j++)
+			{
+				cout << arrA[i][j] << "\t";
+			}
+			cout << endl << endl;
+		}
+		cout << "1.¿ÞÂÊ 2.¿À¸¥ÂÊ" << endl;
+		cin >> Input;
+		if (Input == 1)
+		{
+			for (int i = 0; i < Size; i++)
+			{
+				for (int j = 0; j < Size; j++)
+				{
+					arrB[i][j] = arrA[j][Size - 1 - i];
+				}
+			}
+			for (int i = 0; i < Size; i++)
+			{
+				for (int j = 0; j < Size; j++)
+				{
+					arrA[i][j] = arrB[i][j];
+				}
+			}
+		}
+		else if (Input == 2)
+		{
+			for (int i = 0; i < Size; i++)
+			{
+				for (int j = 0; j < Size; j++)
+				{
+					arrB[i][j] = arrA[Size - 1 - j][i];
+				}
+			}
+			for (int i = 0; i < Size; i++)
+			{
+				for (int j = 0; j < Size; j++)
+				{
+					arrA[i][j] = arrB[i][j];
+				}
+			}
+		}
+	}
+
+	return 0;
+}
